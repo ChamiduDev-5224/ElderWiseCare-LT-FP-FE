fe75cd6bc759bbc48a0c4f7ef239685aac97a4fd
Rachitha95 <rachitharangana9522@gmail.com>
2024-06-20 12:25:42 +0530

    button bottom margin added

--- src/components/home/SectionOne.tsx
+++ src/components/home/SectionOne.tsx
@@ -7,7 +7,7 @@ export const SectionOne = () => {
             <div className='flex flex-col lg:flex-row justify-evenly'>
                 <div className='intro px-8 w-[100%] lg:ml-12 lg:w-[50%]'>
                     <p className='text-light-green pt-10 overflow-visible w-[100%] text-lg'>At ElderWiseCare, we understand the importance of finding reliable and compassionate caregivers for your loved ones. Our intuitive platform connects you with certified and experienced caregivers who are dedicated to providing exceptional elder care. Whether you need daily assistance, specialized medical support, or companionship, our advanced matching system ensures you find the perfect caregiver to meet your needs. Join our community and experience peace of mind, knowing that quality care is just a few clicks away.</p>
-                    <button className='bg-mid-green px-2 py-2 mt-10 rounds'>Learn More</button>
+                    <button className='bg-mid-green px-2 py-2 mt-10 rounds mb-6 hover:bg-mid-green'>Learn More</button>
                 </div>
                 <div className='flex h-[520px]'>
                     <img src={banner1} alt="banner" style={{
