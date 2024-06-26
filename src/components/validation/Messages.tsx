6142e7a8d1ebebbff9c8c3cbbf463a0b2174a0cf
Rachitha95 <rachitharangana9522@gmail.com>
2024-06-26 13:51:15 +0530

    common message file added

---
+++ src/components/validation/Messages.tsx
@@ -0,0 +1,7 @@
+export const Msgs = {
+    'emailReq': 'Email is required.',
+    'pwdReq': 'Password is required.',
+    'usernReq': "UserName is required",
+    'pwdLength': 'Password must be at least 8 characters.',
+    'emilValid': 'Email is not valid.'
+}
