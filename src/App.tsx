46ae8563534df5b227cae791717c92f828f41547
Rachitha95 <rachitharangana9522@gmail.com>
2024-07-28 19:51:53 +0530

    Route update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-
+import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './components/home/Home'
+import { Home } from './components/home/Home';
 import Signup from './components/signup/Signup';
 import { Login } from './components/login/Login';
-import { Provider } from 'react-redux';
-import { Store } from './redux/Store';
 import NotFound from './components/common/NotFound';
-function App() {
+import RegisterForm from './components/registerForm/RegisterForm';
+import ProtectedRoute from './Routes/ProtectedRoute';
+import { Store } from './redux/Store';
+import Entertainment from './components/entertainment/Entertainment';
+import Product from './components/product/Product';
+import ProductDetail from './components/product/ProductDetails';
+import ShoppingCart from './components/cart/ShoppingCart';
+import FAQPag from './components/faq/FAQPag';
+import AboutUs from './components/aboutUs/AboutUs';
+import Service from './components/services/Service';
 
-  return (
-    <Provider store={Store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </Provider>
-  )
+function App() {
+    return (
+        <Provider store={Store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signin" element={<Login />} />
+                    <Route path="/entertainment" element={<Entertainment />} />
+                    <Route path="/about-us" element={<AboutUs />} />
+                    <Route path="/faq" element={<FAQPag />} />
+                    <Route path="/regform" element={<RegisterForm />} />
+                    <Route element={<ProtectedRoute />}>
+                        <Route path="/product" element={<Product />} />
+                        <Route path="/product/:id" element={<ProductDetail />} />
+                        <Route path="/shopping-cart" element={<ShoppingCart />} />
+                        <Route path="/services" element={<Service />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </Provider>
+    );
 }
 
-export default App
+export default App;
